cac007a749561ae2615c5d33a1d89d2abead5861
@LaibaAshfaq2 <laibaashfaqahmed232@gmail.com>
2025-02-07 19:18:19 +0500

    update

--- app/components/protected/page.tsx
+++ app/components/protected/page.tsx
@@ -1,22 +1,16 @@
-'use client';
-
+'use client'
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
-export default function ProtectedRouter({ children }: { children: React.ReactNode }) {
-    const router = useRouter();
-    const [isClient, setIsClient] = useState(false);
+export default function ProtectedRouter({children}:{children:React.ReactNode}){
+    const router = useRouter()
 
-    useEffect(() => {
-        setIsClient(true); // Ensure it's client-side before checking localStorage
-
-        const isLoggedIn = localStorage.getItem("isLoggedIn");
-        if (!isLoggedIn) {
-            router.replace("/admin/dashboard"); // Use `replace` to prevent back navigation
+    useEffect(()=>{
+        const isLoggedIn = localStorage.getItem("isLoggedIn")
+        if (!isLoggedIn){
+            router.push("/admin/dashboard")
         }
-    }, [router]);
+    },[router])
 
-    if (!isClient) return null; // Prevent SSR-related issues
-
-    return <>{children}</>;
+    return<>{children}</>
 }
